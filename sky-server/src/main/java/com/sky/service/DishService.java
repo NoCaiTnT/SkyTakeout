fbed6ee4560c0b58d4aacc51508fc4ce01835dfa
NoCai <1154266678@qq.com>
2024-09-11 10:47:28 +0800

    套餐管理相关功能代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,6 +2,7 @@ package com.sky.service;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
 
@@ -34,4 +35,11 @@ public interface DishService {
      * 更新菜品和对应的口味
      */
     void updateDishAndFlavor(DishDTO dishDTO);
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
 }
